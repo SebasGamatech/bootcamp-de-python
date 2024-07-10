316a166c3d5083d3ee25d5597d11cce22f6104ff
Oscar Sebastian Guerra Erazo <sebastianguerra90@hotmail.com>
2024-07-10 18:56:13 -0500

    Se inicializa el repositorio de python

---
+++ tipos-de-datos.py
@@ -0,0 +1,15 @@
+"string" #Se utiliza para tipos de datos de texto
+'string'
+"""string"""
+'''string'''
+"""tus datos son
+    Nombre: Oscar
+    Apellido: Galvis"""
+#Tipos de datos numéricos
+4 #Tipo de dato entero
+4.5 #Tipo de dato flotante
+
+#Tipos de datos booleanos
+True # con T Mayúscula
+False # con F Mayúscula
+
