96a5d25b045ad2a55a74521e0ca7361236c7a47e
Oscar Sebastian Guerra Erazo <sebastianguerra90@hotmail.com>
2024-07-10 20:47:23 -0500

    Avanzamos variables

---
+++ variables/variables.py
@@ -0,0 +1,15 @@
+nombre = "Ricardo"
+bienvenida = f"Hola {nombre} feliz noche"
+
+print(bienvenida)
+print("ola" in bienvenida)
+print("Betty" not in bienvenida)
+print("Hola" not in bienvenida)
+
+#CamelCase
+nombreCompletoDeTuProfe = "Jheyson Galvis"
+
+#SnakeCase - Es la que debemos utilizar en python
+nombre_completo_de_tu_profe = "Jheyson Galvis"
+
+
