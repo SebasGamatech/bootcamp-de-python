497dbe9b153a2eb8dd05b4c3e2da2985830a0594
Oscar Sebastian Guerra Erazo <sebastianguerra90@hotmail.com>
2024-09-10 22:00:45 -0500

    Abordamos inputs

---
+++ inputs/input_numeros.py
@@ -0,0 +1,7 @@
+#Pedirle un número al usuario
+numero = input("Dame un número campista:")
+#multiplicando el número por 2
+#Convirtiendo el número entero a float
+resultado = float(numero)*2
+#Mostrando el resultado
+print(resultado)
---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+#Pedirle un dato al usuario
+nombre = input("¿Cual es tu nombre?: ")
+#mostrando el dato
+print(f'el nombre es {nombre}')
+print(type(nombre))
